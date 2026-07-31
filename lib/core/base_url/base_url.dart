5d3d41ece3507f257c2c3e01ec7396bba937bd4b
Hasanuzzaman Abir <hasanabir72@gmail.com>
2026-07-31 14:46:15 +0600

    chat system correction

--- lib/core/base_url/base_url.dart
+++ lib/core/base_url/base_url.dart
@@ -1,7 +1,7 @@
 class BaseUrl {
-  //static const String baseUrl = 'https://api.amorerings.com';
-  static const String baseUrl = 'https://clashingly-nonlicensable-tennille.ngrok-free.dev';
+  static const String baseUrl = 'https://api.amorerings.com';
+  //static const String baseUrl = 'https://clashingly-nonlicensable-tennille.ngrok-free.dev';
 
-  //static const String webSocketUrl = 'wss://api.amorerings.com/ws/chat';
-  static const String webSocketUrl = 'wss://clashingly-nonlicensable-tennille.ngrok-free.dev/ws/chat';
+  static const String webSocketUrl = 'wss://api.amorerings.com/ws/chat';
+ //static const String webSocketUrl = 'wss://clashingly-nonlicensable-tennille.ngrok-free.dev/ws/chat';
 }
